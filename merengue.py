483fa8399d6599184fd1d464e18459359ce6f28e
John Carter <jcarter2010@comcast.net>
2016-07-24 22:33:09 -0500

    Fixed folder tree and added variable finding

--- merengue.py
+++ merengue.py
@@ -12,6 +12,7 @@ from tkMessageBox import *
 import keyword
 import tkFileDialog
 import tkMessageBox
+#from pygments.lexers import PythonLexer
 
 
 class EditorClass(object):
@@ -61,6 +62,8 @@ class EditorClass(object):
         self.text.bind('<5>', self.syntax_coloring)
         self.text.bind('<Tab>', self.tab)
         self.text.bind('<Return>', self.enter)
+        self.text.bind('<Escape>', self.remove_highlight)
+        self.text.bind('<Control-q>', self.highlight_variable)
 
     def enter(self, event):
         start = float(int(float(self.text.index(INSERT))))
@@ -294,7 +297,7 @@ class EditorClass(object):
             self.text.mark_set("searchLimit", end)
             count = tk.IntVar()
             while True:
-                index = self.text.search('".*"', "matchEnd", "searchLimit", count=count, regexp=regexp)
+                index = self.text.search(r'"(.*?)"', "matchEnd", "searchLimit", count=count, regexp=regexp)
                 if index == "": break
                 if count.get() == 0: break
                 self.text.mark_set("matchStart", index)
@@ -307,7 +310,7 @@ class EditorClass(object):
             self.text.mark_set("searchLimit", end)
             count = tk.IntVar()
             while True:
-                index = self.text.search("'.*'", "matchEnd", "searchLimit", count=count, regexp=regexp)
+                index = self.text.search(r"'(.*?)'", "matchEnd", "searchLimit", count=count, regexp=regexp)
                 if index == "": break
                 if count.get() == 0: break
                 self.text.mark_set("matchStart", index)
@@ -315,6 +318,7 @@ class EditorClass(object):
                 self.text.tag_add(tag, "matchStart", "matchEnd")
 
     def remove_highlight(self, pattern, tag, start="1.0", end="end", regexp=False):
+        '''
         start = self.text.index(start)
         end = self.text.index(end)
         self.text.mark_set("matchStart", start)
@@ -328,6 +332,25 @@ class EditorClass(object):
             self.text.mark_set("matchStart", index)
             self.text.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
             self.text.tag_remove(tag, "matchStart", "matchEnd")
+        '''
+        self.text.tag_remove('highlight', '1.0', END)
+
+    def highlight_variable(self, event):
+        pattern = self.text.get(tk.SEL_FIRST, tk.SEL_LAST)
+        tag = "highlight"
+        start = self.text.index('1.0')
+        end = self.text.index(END)
+        self.text.mark_set("matchStart", start)
+        self.text.mark_set("matchEnd", start)
+        self.text.mark_set("searchLimit", end)
+        count = tk.IntVar()
+        while True:
+            index = self.text.search('".*(?:'+pattern+').*"|('+pattern+')', "matchEnd", "searchLimit", count=count, regexp=True)
+            if index == "": break
+            if count.get() == 0: break
+            self.text.mark_set("matchStart", index)
+            self.text.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
+            self.text.tag_add(tag, "matchStart", "matchEnd")
 
 class Tree_Node:
     global name
@@ -340,7 +363,7 @@ class Tree_Node:
 class App:
 
     def open_file(self, path):
-        if self.check_if_file(path):
+        if isfile(path):
             if not path in self.tab_names:
                 pane = PanedWindow(self.n, orient=HORIZONTAL, opaqueresize=True)
                 ed = EditorClass(self.root, path)
@@ -363,19 +386,8 @@ class App:
                 ed.text.tag_configure("number", foreground='cyan')
                 ed.text.tag_configure("operator", foreground='blue')
                 ed.text.tag_configure('normal', foreground='white')
-                #ed.text.syntax_coloring(None)
                 self.eds.append(ed)
             self.n.select(self.tab_names.index(path))
-
-    def find_path(self, path, node, fname):
-        if fname == node.name:
-            return(node.name, True)
-        for tree_node in node.nodes:
-            path_temp, found = self.find_path(node.name, tree_node, fname)
-            if found:
-                path = node.name + '/' + path_temp
-                return path, True
-        return '', False
 
     def copy_click(self):
         index = self.n.tabs().index(self.n.select())
@@ -393,52 +405,31 @@ class App:
         text = self.eds[index].text.selection_get(selection='CLIPBOARD')
         self.eds[index].text.insert('insert', text)
 
-    def check_if_file(self, path):
-        temp_path = path[:path.rfind('/')]
-        fname = path[path.rfind('/')+1:]
-        if temp_path == '':
-            temp_path = '.'
-        print(temp_path + '|' + path)
-        files = [f for f in listdir(temp_path) if isfile(join(temp_path, f))]
-        if fname in files:
-            return True
-        return False
+    def recursive_find(self, rootDir):
+        for lists in os.listdir(rootDir):
+            path = os.path.join(rootDir, lists)
+            self.files.append(path)
+            if os.path.isdir(path):
+                self.recursive_find(path)
 
     def list_files(self, path, tree, parent, full_path):
-    	try:
-            if path == '.':
-                tree.insert('', 3, full_path + '/' + path, text=path, tags = ('directory',))
-            else:
-                tree.insert(full_path, 3, full_path + '/'+ path, text=path, tags = ('directory',))
-        except:
-            print('already exitsts')
-        files = [f for f in listdir(full_path+'/'+path) if isfile(join(full_path+'/'+path, f))]
-        dirs = [d for d in listdir(full_path+'/'+path) if not isfile(join(full_path+'/'+path, d))]
-        tn = Tree_Node(path)
-        for d in dirs:
-            try:
-                if parent != '':
-                    tree, n2 = self.list_files(d, tree, path, full_path + '/' + path)
+        self.files = [os.getcwd()]
+        self.recursive_find(os.getcwd())
+        counter = 0
+        for f in self.files:
+            if counter != 0:
+                if(isfile(f)):
+                    tree.insert(f[:f.rfind('/')], 3, f, text=f[f.rfind('/') + 1:], tags = ('file',))
                 else:
-                    tree, n2 = self.list_files(d, tree, path, full_path + '/' + path)
-                tn.nodes.append(n2)
-            except:
-                print('already exitsts')
-        for f in files:
-                try:
-                    tree.insert(full_path + '/' + path, 3, full_path + '/' + f, text=f, tags = ('file',))
-                    tn.nodes.append(Tree_Node(f))
-                except:
-                    print('already exitsts')
-        return tree, tn
+                    tree.insert(f[:f.rfind('/')], 3, f, text=f[f.rfind('/') + 1:], tags = ('directory',))
+            else:
+                tree.insert('', 3, f, text=f[f.rfind('/') + 1:], tags = ('directory',))
+            counter = counter + 1
+        return tree
 
     def on_double_click(self, event):
         item = self.tree.selection()[0]
-        path, found = self.find_path('', self.tree_array, item[item.rfind('/')+1:])
-        print(found)
-        if found:
-            print('found')
-            self.open_file(path)
+        self.open_file(item)
 
 
     def close_all_tabs(self):
@@ -476,8 +467,8 @@ class App:
         with open(path, 'w') as f_out:
             f_out.write(self.eds[index].text.get("1.0",END))
         self.tree.delete(*self.tree.get_children())
-        self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-        self.tree.item("./.", open=True)
+        self.tree = self.list_files('.', self.tree, "", '.')
+        self.tree.item(os.getcwd(), open=True)
 
     def save_type(self, event):
         path = self.n.tab(self.n.select())['text']
@@ -486,8 +477,8 @@ class App:
         with open(path, 'w') as f_out:
             f_out.write(self.eds[index].text.get("1.0",END))
         self.tree.delete(*self.tree.get_children())
-        self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-        self.tree.item("./.", open=True)
+        self.tree = self.list_files('.', self.tree, "", '.')
+        self.tree.item(os.getcwd(), open=True)
 
     def exit_click(self):
         sys.exit()
@@ -503,12 +494,12 @@ class App:
             folder = tkFileDialog.askdirectory()
             os.chdir(folder)
             self.tree.delete(*self.tree.get_children())
-            self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-            self.tree.item("./.", open=True)
+            self.tree = self.list_files('.', self.tree, "", '.')
+            self.tree.item(os.getcwd(), open=True)
 
     def find_type(self, event):
         path = self.n.tab(self.n.select())['text']
-        args = ['python2', 'find_and_replace.py']
+        args = ['python2', self.merengue_path + '/' + 'find_and_replace.py']
         p = Popen(args, stdin=PIPE, stdout=PIPE, shell=False)
         p.wait()
         out = p.stdout.read().replace('\n', '')
@@ -532,10 +523,10 @@ class App:
                     index = index + 1
 
     def tree_rename(self):
-        item = self.selected_file_dir
+        item = self.tree.selection()[0]
         path, found = self.find_path('.', self.tree_array, item)
         if found:
-            args = ['python2', 'rename.py', 'test']
+            args = ['python2', self.merengue_path + '/' + 'rename.py', 'test']
             p = Popen(args, stdin=PIPE, stdout=PIPE, shell=False)
             p.wait()
             out = p.stdout.read().replace('\n', '')
@@ -550,24 +541,22 @@ class App:
                 except:
                     print('file does not exist, not renaming anything but the tab')
         self.tree.delete(*self.tree.get_children())
-        self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-        self.tree.item("./.", open=True)
+        self.tree = self.list_files('.', self.tree, "", '.')
+        self.tree.item(os.getcwd(), open=True)
 
     def delete_file(self):
-        item = self.selected_file_dir
-        path, found = self.find_path('.', self.tree_array, item)
-        if found:
-            try:
-                os.remove(path)
-            except:
-                print('Not a file')
-            try:
-                os.rmdir(path)
-            except:
-                print('Not a directory')
+        item = self.tree.selection()[0]
+        try:
+            os.remove(item)
+        except:
+            print('Not a file')
+        try:
+            os.rmdir(item)
+        except:
+            print('Not a directory')
         self.tree.delete(*self.tree.get_children())
-        self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-        self.tree.item("./.", open=True)
+        self.tree = self.list_files('.', self.tree, "", '.')
+        self.tree.item(os.getcwd(), open=True)
 
     def show_menu(self, event):
         self.directory_menu.post(event.x_root, event.y_root)
@@ -579,7 +568,7 @@ class App:
 
     def tab_rename(self, event):
         path = self.n.tab(self.n.select())['text']
-        args = ['python2', 'rename.py', path[path.rfind('/')+1:]]
+        args = ['python2', self.merengue_path + '/' + 'rename.py', path[path.rfind('/')+1:]]
         p = Popen(args, stdin=PIPE, stdout=PIPE, shell=False)
         p.wait()
         out = p.stdout.read().replace('\n', '')
@@ -606,8 +595,8 @@ class App:
         self.pane.add(ed.frame)
         self.eds.append(ed)
         self.tree = ttk.Treeview(self.root)
-        self.tree, self.tree_array = self.list_files('.', self.tree, "", '.')
-        self.tree.item("./.", open=True)
+        self.tree = self.list_files('.', self.tree, "", '.')
+        self.tree.item(os.getcwd(), open=True)
         self.tree.tag_configure('directory', background='black', foreground='magenta')
         self.tree.tag_configure('file', background='black', foreground='lime')
         ttk.Style().configure("Treeview", fieldbackground="black")
@@ -646,6 +635,8 @@ class App:
         self.directory_menu.add_command(label="Rename", command=self.tree_rename)
 
     def __init__(self):
+        self.merengue_path = os.path.realpath(__file__)
+        self.merengue_path = self.merengue_path[:self.merengue_path.rfind('/')]
     	os.chdir(os.path.join(os.path.expanduser('~'), 'Documents'))
         self.root = Tk()
         self.eds = []
