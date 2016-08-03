acb535c9870e1c80433860bb3a71ae66363807db
John Carter <jcarter2010@comcast.net>
2016-08-03 00:05:32 -0500

    Added setup script for test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='merengue',
     entry_points={
-        'console_scripts': ['merengue = merengue:main']},
+        'console_scripts': ['merengue = merengue.merengue:main']},
     version='1.0.0',
     author='John Carter',
     author_email='jcarter2010@comcast.net',
