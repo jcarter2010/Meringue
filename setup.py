a4db9232e6ededbb69af6d943b4674db0b9de53e
John Carter <jcarter2010@comcast.net>
2016-08-03 00:12:12 -0500

    Changed setup for a test

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='merengue',
     entry_points={
-        'console_scripts': ['merengue = merengue.merengue:main']},
-    version='1.0.1',
+        'console_scripts': ['merengue = merengue.__main__:main']},
+    version='1.0.2',
     author='John Carter',
     author_email='jcarter2010@comcast.net',
     packages=['merengue'],
