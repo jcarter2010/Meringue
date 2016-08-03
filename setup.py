05c780e8b75445b964c74765b54254c468087aa4
John Carter <jcarter2010@comcast.net>
2016-08-03 00:17:35 -0500

    Changed setup for a test

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='merengue',
+    install_requires=['paramiko'],
     entry_points={
         'qui_scripts': ['merengue = merengue.__main__:main']},
-    version='1.0.3',
+    version='1.0.4',
     author='John Carter',
     author_email='jcarter2010@comcast.net',
     packages=['merengue'],
